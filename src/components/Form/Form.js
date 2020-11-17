7dff18cdc6a305eb5c703613d2f87982a2767dc4
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2020-11-17 21:20:42 +0100

    handle form using custom methods

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,11 +2,39 @@ import React, { Component } from "react";
 import "./Form.scss";
 
 class Form extends Component {
+  state = {
+    counter: 0,
+    type: "paragraphs"
+  };
+
+  sendForm = e => {
+    e.preventDefault();
+  };
+
+  getCounter = e => {
+    this.setState({ counter: e.target.value });
+  };
+
+  getType = e => {
+    this.setState({ type: e.target.value });
+  };
+
   render() {
     return (
-      <form className="form">
-        <input type="number" min="0" className="form__input" value="0" />
-        <select name="type" className="form__select">
+      <form className="form" onSubmit={this.sendForm}>
+        <input
+          type="number"
+          min="0"
+          className="form__input"
+          onChange={this.getCounter}
+          value={this.state.counter}
+        />
+        <select
+          name="type"
+          className="form__select"
+          onChange={this.getType}
+          value={this.state.type}
+        >
           <option value="paragraphs" className="form__option">
             paragraphs
           </option>
