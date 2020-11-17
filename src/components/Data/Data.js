7a4ecf2df07b1bd0fd0df74a53885f52a08b2a10
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2020-11-17 21:18:51 +0100

    destruct props and iterate items passing single prop

--- src/components/Data/Data.js
+++ src/components/Data/Data.js
@@ -1,12 +1,16 @@
 import React from "react";
+import DataItem from "../DataItem/DataItem";
 import "./Data.scss";
 
-const Data = () => {
+const Data = ({ paragraphs, sentences, words }) => {
   return (
     <section className="data">
       <h2 className="data__title">
-        You generated 0 paragraphs
+        You generated {sentences.length + 1} paragraphs
         <span className="data__copy">copy</span>
+        {sentences.map(sentence => (
+          <DataItem item={sentence} />
+        ))}
       </h2>
       {/* <h2 className="data__title hidden">Your page is empty!</h2> */}
     </section>
