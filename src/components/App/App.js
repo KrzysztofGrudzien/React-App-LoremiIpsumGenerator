c8d291f0a64b6843fe208f4cb45ef8dd2fec04c0
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2020-11-18 21:37:31 +0100

    move form element and style to app component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Form from "../Form/Form";
 import Header from "../Header/Header";
 import ArticleDecoration from "../ArticleDecoration/ArticleDecoration";
 import Data from "../Data/Data";
@@ -9,15 +8,58 @@ class App extends Component {
   state = {
     paragraphs: [...data.paragraphs],
     sentences: [...data.sentences],
-    words: [...data.words]
+    words: [...data.words],
+    counter: 0,
+    type: "paragraphs"
   };
+
+  sendForm = e => {
+    e.preventDefault();
+    console.log(this.state.counter);
+  };
+
+  getCounter = e => {
+    this.setState({ counter: e.target.value });
+  };
+
+  getType = e => {
+    this.setState({ type: e.target.value });
+  };
+
   render() {
     return (
       <main className="main">
         <section className="app">
           <Header />
           <ArticleDecoration />
-          <Form />
+          <form className="form" onSubmit={this.sendForm}>
+            <input
+              type="number"
+              min="0"
+              className="form__input"
+              onChange={this.getCounter}
+              value={this.state.counter}
+            />
+            <select
+              name="type"
+              className="form__select"
+              onChange={this.getType}
+              value={this.state.type}
+            >
+              <option value="paragraphs" className="form__option">
+                paragraphs
+              </option>
+              <option value="sentences" className="form__option">
+                sentences
+              </option>
+              <option value="words" className="form__option">
+                words
+              </option>
+            </select>
+            <button type="submit" className="form__button">
+              generate
+            </button>
+          </form>
           <Footer />
         </section>
         <Data
